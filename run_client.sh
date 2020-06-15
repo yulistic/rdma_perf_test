0be008b0da7c4bb5b40331b15a641a21778e397b
Jongyul Kim <yulistic@gmail.com>
2020-06-15 16:15:06 +0900

    Modify script

--- run_client.sh
+++ run_client.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-REMOTE="bull2"      # Remote hostname.
-LOCAL="bull5"       # Local hostname.
-IP_REMOTE="192.168.13.115"  # Remote host ip address.
+REMOTE="libra04"      # Remote hostname.
+LOCAL="libra05"       # Local hostname.
+IP_REMOTE="libra04-rdma"  # Remote host ip address.
 
 TYPE="read write send"
 
+OUT_DIR="results"
+mkdir -p $OUT_DIR
+
 for type in $TYPE
 do
-    ib_${type}_lat -d mlx5_0 -i 1 -a -F $IP_REMOTE | tee ${type}_lat_${LOCAL}_to_${REMOTE}.txt
-    ib_${type}_lat -d mlx5_0 -i 1 -a -F | tee ${type}_lat_${REMOTE}_to_${LOCAL}.txt
+    ib_${type}_lat -d mlx5_0 -i 1 -a -F $IP_REMOTE | tee ${OUT_DIR}/${type}_lat_${LOCAL}_to_${REMOTE}.txt
+    ib_${type}_lat -d mlx5_0 -i 1 -a -F | tee ${OUT_DIR}/${type}_lat_${REMOTE}_to_${LOCAL}.txt
     sleep 2
 done
 
 for type in $TYPE
 do
-    ib_${type}_bw -d mlx5_0 -i 1 -a -F $IP_REMOTE | tee ${type}_bw_${LOCAL}_to_${REMOTE}.txt
-    ib_${type}_bw -d mlx5_0 -i 1 -a -F | tee ${type}_bw_${REMOTE}_to_${LOCAL}.txt
+    ib_${type}_bw -d mlx5_0 -i 1 -a -F $IP_REMOTE | tee ${OUT_DIR}/${type}_bw_${LOCAL}_to_${REMOTE}.txt
+    ib_${type}_bw -d mlx5_0 -i 1 -a -F | tee ${OUT_DIR}/${type}_bw_${REMOTE}_to_${LOCAL}.txt
     sleep 2
 done
